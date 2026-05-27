38eb30841bdaddccf2e7c35d35516ab9cf98abbe
Aldair Lara <aldairlara@gmail.com>
2026-05-27 02:05:57 -0600

    feat(MISSION_BOSS_v2): add project

--- MISSION_BOSS_v2/src/NVIC.c
+++ MISSION_BOSS_v2/src/NVIC.c
@@ -66,25 +66,25 @@ extern uint8_t MASTER_DataReceived_STOP_BYTE;
 void UART0_Handler(void) {
 
     /** Validación del comando recibido */
-    PC_DataReceived_SCMI_SUBSYSTEM = (UART0_DR_R & 0xFF);
-    PC_DataReceived_SUBSYSTEM_ID = (UART0_DR_R & 0xFF);
+    PC_DataReceived_SCMI_SUBSYSTEM = (UART0_DR_R & UART_DR_DATA_M);
+    PC_DataReceived_SUBSYSTEM_ID = (UART0_DR_R & UART_DR_DATA_M);
     while (UART0_FR_R & UART_FR_RXFE) {}
-    PC_DataReceived_COMMAND_ID = (UART0_DR_R & 0xFF);
+    PC_DataReceived_COMMAND_ID = (UART0_DR_R & UART_DR_DATA_M);
     while (UART0_FR_R & UART_FR_RXFE) {}
-    PC_DataReceived_DATA_SIZE = (UART0_DR_R & 0xFF);
+    PC_DataReceived_DATA_SIZE = (UART0_DR_R & UART_DR_DATA_M);
 
     int i = 0;
     for (i = 0; i < PC_DataReceived_DATA_SIZE; i++) {
         while (UART0_FR_R & UART_FR_RXFE) {}
-        PC_DataReceived_DATA[i] = (UART0_DR_R & 0xFF);
+        PC_DataReceived_DATA[i] = (UART0_DR_R & UART_DR_DATA_M);
     }
 
     while (UART0_FR_R & UART_FR_RXFE) {}
-    PC_DataReceived_CRC_MSB = (UART0_DR_R & 0xFF);
+    PC_DataReceived_CRC_MSB = (UART0_DR_R & UART_DR_DATA_M);
     while (UART0_FR_R & UART_FR_RXFE) {}
-    PC_DataReceived_CRC_LSB = (UART0_DR_R & 0xFF);
+    PC_DataReceived_CRC_LSB = (UART0_DR_R & UART_DR_DATA_M);
     while (UART0_FR_R & UART_FR_RXFE) {}
-    PC_DataReceived_STOP_BYTE = (UART0_DR_R & 0xFF);
+    PC_DataReceived_STOP_BYTE = (UART0_DR_R & UART_DR_DATA_M);
 
     /** Limpieza de la bandera de interrupción */
     UART0_ICR_R = UART_ICR_RXIC;                                                                    /*  UART0 => RXIC: Receive Interrupt Clear -> RXRIS bit (UARTRIS) and RXMIS bit (UARTMIS) cleared */
@@ -144,23 +144,23 @@ void UART0_Handler(void) {
 void UART4_Handler(void) {
 
     /** Validación del comando recibido */
-    SUPERVISOR_DataReceived_SUBSYSTEM_ID = (UART4_DR_R & 0xFF);
-    SUPERVISOR_DataReceived_COMMAND_ID = (UART4_FR_R & 0xFF);
+    SUPERVISOR_DataReceived_SUBSYSTEM_ID = (UART4_DR_R & UART_DR_DATA_M);
+    SUPERVISOR_DataReceived_COMMAND_ID = (UART4_DR_R & UART_DR_DATA_M);
     while (UART4_FR_R & UART_FR_RXFE) {}
-    SUPERVISOR_DataReceived_DATA_SIZE = (UART4_FR_R & 0xFF);
+    SUPERVISOR_DataReceived_DATA_SIZE = (UART4_DR_R & UART_DR_DATA_M);
 
     int i = 0;
     for (i = 0; i < SUPERVISOR_DataReceived_DATA_SIZE; i++) {
         while (UART4_FR_R & UART_FR_RXFE) {}
-        SUPERVISOR_DataReceived_DATA[i] = (UART4_FR_R & 0xFF);
+        SUPERVISOR_DataReceived_DATA[i] = (UART4_DR_R & UART_DR_DATA_M);
     }
 
     while (UART4_FR_R & UART_FR_RXFE) {}
-    SUPERVISOR_DataReceived_CRC_MSB = (UART4_FR_R & 0xFF);
+    SUPERVISOR_DataReceived_CRC_MSB = (UART4_DR_R & UART_DR_DATA_M);
     while (UART4_FR_R & UART_FR_RXFE) {}
-    SUPERVISOR_DataReceived_CRC_LSB = (UART4_FR_R & 0xFF);
+    SUPERVISOR_DataReceived_CRC_LSB = (UART4_DR_R & UART_DR_DATA_M);
     while (UART4_FR_R & UART_FR_RXFE) {}
-    SUPERVISOR_DataReceived_STOP_BYTE = (UART4_FR_R & 0xFF);
+    SUPERVISOR_DataReceived_STOP_BYTE = (UART4_DR_R & UART_DR_DATA_M);
 
     /** Limpieza de la bandera de interrupción */
     UART4_ICR_R = UART_ICR_RXIC;                                                                    /*  UART0 => RXIC: Receive Interrupt Clear -> RXRIS bit (UARTRIS) and RXMIS bit (UARTMIS) cleared */
@@ -168,23 +168,23 @@ void UART4_Handler(void) {
     /** Envío de la trama al PC */
     uint8_t j;
     while (UART4_FR_R & UART_FR_TXFF) {}
-    UART4_DR_R = (SUPERVISOR_DataReceived_SUBSYSTEM_ID & UART_DR_DATA_M);
+    UART0_DR_R = (SUPERVISOR_DataReceived_SUBSYSTEM_ID & UART_DR_DATA_M);
     while (UART4_FR_R & UART_FR_TXFF) {}
-    UART4_DR_R = (SUPERVISOR_DataReceived_COMMAND_ID & UART_DR_DATA_M);
+    UART0_DR_R = (SUPERVISOR_DataReceived_COMMAND_ID & UART_DR_DATA_M);
     while (UART4_FR_R & UART_FR_TXFF) {}
-    UART4_DR_R = (SUPERVISOR_DataReceived_DATA_SIZE & UART_DR_DATA_M);
+    UART0_DR_R = (SUPERVISOR_DataReceived_DATA_SIZE & UART_DR_DATA_M);
 
     for (j = 0; j < SUPERVISOR_DataReceived_DATA_SIZE; j++) {
         while (UART4_FR_R & UART_FR_TXFF) {}
-        UART4_DR_R = (SUPERVISOR_DataReceived_DATA[j] & UART_DR_DATA_M);
+        UART0_DR_R = (SUPERVISOR_DataReceived_DATA[j] & UART_DR_DATA_M);
     }
 
     while (UART4_FR_R & UART_FR_TXFF) {}
-    UART4_DR_R = (SUPERVISOR_DataReceived_CRC_MSB & UART_DR_DATA_M);
+    UART0_DR_R = (SUPERVISOR_DataReceived_CRC_MSB & UART_DR_DATA_M);
     while (UART4_FR_R & UART_FR_TXFF) {}
-    UART4_DR_R = (SUPERVISOR_DataReceived_CRC_LSB & UART_DR_DATA_M);
+    UART0_DR_R = (SUPERVISOR_DataReceived_CRC_LSB & UART_DR_DATA_M);
     while (UART4_FR_R & UART_FR_TXFF) {}
-    UART4_DR_R = (SUPERVISOR_DataReceived_STOP_BYTE & UART_DR_DATA_M);
+    UART0_DR_R = (SUPERVISOR_DataReceived_STOP_BYTE & UART_DR_DATA_M);
 }
 
 /************************************************
@@ -196,23 +196,23 @@ void UART4_Handler(void) {
 void UART7_Handler(void) {
 
     /** Validación del comando recibido */
-    MASTER_DataReceived_SUBSYSTEM_ID = (UART7_DR_R & 0xFF);
-    MASTER_DataReceived_COMMAND_ID = (UART7_FR_R & 0xFF);
+    MASTER_DataReceived_SUBSYSTEM_ID = (UART7_DR_R & UART_DR_DATA_M);
+    MASTER_DataReceived_COMMAND_ID = (UART7_DR_R & UART_DR_DATA_M);
     while (UART7_FR_R & UART_FR_RXFE) {}
-    MASTER_DataReceived_DATA_SIZE = (UART7_FR_R & 0xFF);
+    MASTER_DataReceived_DATA_SIZE = (UART7_DR_R & UART_DR_DATA_M);
 
     int i = 0;
     for (i = 0; i < MASTER_DataReceived_DATA_SIZE; i++) {
         while (UART7_FR_R & UART_FR_RXFE) {}
-        MASTER_DataReceived_DATA[i] = (UART7_FR_R & 0xFF);
+        MASTER_DataReceived_DATA[i] = (UART7_DR_R & UART_DR_DATA_M);
     }
 
     while (UART7_FR_R & UART_FR_RXFE) {}
-    MASTER_DataReceived_CRC_MSB = (UART7_FR_R & 0xFF);
+    MASTER_DataReceived_CRC_MSB = (UART7_DR_R & UART_DR_DATA_M);
     while (UART7_FR_R & UART_FR_RXFE) {}
-    MASTER_DataReceived_CRC_LSB = (UART7_FR_R & 0xFF);
+    MASTER_DataReceived_CRC_LSB = (UART7_DR_R & UART_DR_DATA_M);
     while (UART7_FR_R & UART_FR_RXFE) {}
-    MASTER_DataReceived_STOP_BYTE = (UART7_FR_R & 0xFF);
+    MASTER_DataReceived_STOP_BYTE = (UART7_DR_R & UART_DR_DATA_M);
 
     /** Limpieza de la bandera de interrupción */
     UART7_ICR_R = UART_ICR_RXIC;                                                                    /*  UART0 => RXIC: Receive Interrupt Clear -> RXRIS bit (UARTRIS) and RXMIS bit (UARTMIS) cleared */
@@ -220,21 +220,21 @@ void UART7_Handler(void) {
     /** Envío de la trama al PC */
     uint8_t j;
     while (UART7_FR_R & UART_FR_TXFF) {}
-    UART7_DR_R = (MASTER_DataReceived_SUBSYSTEM_ID & UART_DR_DATA_M);
+    UART0_DR_R = (MASTER_DataReceived_SUBSYSTEM_ID & UART_DR_DATA_M);
     while (UART7_FR_R & UART_FR_TXFF) {}
-    UART7_DR_R = (MASTER_DataReceived_COMMAND_ID & UART_DR_DATA_M);
+    UART0_DR_R = (MASTER_DataReceived_COMMAND_ID & UART_DR_DATA_M);
     while (UART7_FR_R & UART_FR_TXFF) {}
-    UART7_DR_R = (MASTER_DataReceived_DATA_SIZE & UART_DR_DATA_M);
+    UART0_DR_R = (MASTER_DataReceived_DATA_SIZE & UART_DR_DATA_M);
 
     for (j = 0; j < MASTER_DataReceived_DATA_SIZE; j++) {
         while (UART7_FR_R & UART_FR_TXFF) {}
-        UART7_DR_R = (MASTER_DataReceived_DATA[j] & UART_DR_DATA_M);
+        UART0_DR_R = (MASTER_DataReceived_DATA[j] & UART_DR_DATA_M);
     }
 
     while (UART7_FR_R & UART_FR_TXFF) {}
-    UART7_DR_R = (MASTER_DataReceived_CRC_MSB & UART_DR_DATA_M);
+    UART0_DR_R = (MASTER_DataReceived_CRC_MSB & UART_DR_DATA_M);
     while (UART7_FR_R & UART_FR_TXFF) {}
-    UART7_DR_R = (MASTER_DataReceived_CRC_LSB & UART_DR_DATA_M);
+    UART0_DR_R = (MASTER_DataReceived_CRC_LSB & UART_DR_DATA_M);
     while (UART7_FR_R & UART_FR_TXFF) {}
-    UART7_DR_R = (MASTER_DataReceived_STOP_BYTE & UART_DR_DATA_M);
+    UART0_DR_R = (MASTER_DataReceived_STOP_BYTE & UART_DR_DATA_M);
 }
